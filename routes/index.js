d0c9f0c23c54d419a9e6918cbdc27c94ba7a125d
jasmo2 <jasmo2.com@gmail.com>
2016-03-10 12:10:35 -0500

    add routes

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/latidos', function(req, res, next) {
+  res.render('latidos');
+});
+router.get('/', function(req, res, next) {
+  res.render('localizacion');
+});
+router.get('/', function(req, res, next) {
+  res.render('respiracion');
+});
 //router.get('/event', ctrEvents.positionEventCreate);
 
 module.exports = router;
