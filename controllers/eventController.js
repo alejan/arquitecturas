bdd8afba2888ec17153495830c0aec3e1c28b73b
jasmo2 <jasmo2.com@gmail.com>
2016-03-10 18:20:44 -0500

    refactor Controllers

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -5,70 +5,71 @@ var localizacionEvent = mongoose.model('localizacionEvent');
 var respiracionEvent = mongoose.model('respiracionEvent');
 
 var sendJsonResponse = function(res, status, content){
-    res.status(status);
-    res.json(content);
+	res.status(status);
+	res.json(content);
 };
 
-module.exports.positionEventCreate = function(req, res){
-	  positionEvent.create({
-	        petName: "temp",
-	        coords: [parseFloat(10), parseFloat(20)],
-	    }, function(err, location){
-	        if(err){
-	            sendJsonResponse(res, 400, err);
-	        } else {
-	            console.log("saved data");
-	            sendJsonResponse(res, 201, location);
-	        }
-	    });
+module.exports = {
+	positionEventCreate: function(req, res){
+		positionEvent.create({
+			petName: "temp",
+			coords: [parseFloat(10), parseFloat(20)],
+		}, function(err, location){
+			if(err){
+				sendJsonResponse(res, 400, err);
+			} else {
+				console.log("saved data");
+				sendJsonResponse(res, 201, location);
+			}
+		});
 
-	};
+	},
+	latidoEventCreate: function(data){
+		latidoEvent.create({
+			idMascota: data.idMascota,
+			idCollar: data.idCollar,
+			idUsuario: data.idUsuario,
+			latido: data.latido,
+			fecha: data.fecha
+		}, function(err, location){
+			if(err){
+				console.log("saved data latido ERR");
+			} else {
+				console.log("saved data latido OK");
+			}
+		});
+	},
 
-module.exports.latidoEventCreate = function(data){
-	latidoEvent.create({
-	  idMascota: data.idMascota,
-	  idCollar: data.idCollar,
-	  idUsuario: data.idUsuario,
-	  latido: data.latido,
-	  fecha: data.fecha
-    }, function(err, location){
-        if(err){
-            console.log("saved data latido ERR");
-        } else {
-            console.log("saved data latido OK");
-        }
-    });
-};
+	localizacionEventCreate: function(data){
+		localizacionEvent.create({
+			idMascota: data.idMascota,
+			idCollar: data.idCollar,
+			idUsuario: data.idUsuario,
+			latitud: data.latitud,
+			longitud: data.longitud,
+			fecha: data.fecha
+		}, function(err, location){
+			if(err){
+				console.log("saved data localizacion ERR");
+			} else {
+				console.log("saved data localizacion OK");
+			}
+		});
+	},
 
-module.exports.localizacionEventCreate = function(data){
-	localizacionEvent.create({
-	  idMascota: data.idMascota,
-	  idCollar: data.idCollar,
-	  idUsuario: data.idUsuario,
-	  latitud: data.latitud,
-	  longitud: data.longitud,
-	  fecha: data.fecha
-	}, function(err, location){
-        if(err){
-            console.log("saved data localizacion ERR");
-        } else {
-            console.log("saved data localizacion OK");
-        }
-	});
-};
-
-module.exports.respiracionEventCreate = function(data){
-	respiracionEvent.create({
-	  idMascota: data.idMascota,
-	  idCollar: data.idCollar,
-	  idUsuario: data.idUsuario,
-	  respiracion: data.respiracion,
-	  fecha: data.fecha
-    }, function(err, location){
-        if(err){
-            console.log("saved data respiracion ERR");
-        } else {
-            console.log("saved data respiracion OK");
-        }
-    });
+	respiracionEventCreate: function(data){
+		respiracionEvent.create({
+			idMascota: data.idMascota,
+			idCollar: data.idCollar,
+			idUsuario: data.idUsuario,
+			respiracion: data.respiracion,
+			fecha: data.fecha
+		}, function(err, location){
+			if(err){
+				console.log("saved data respiracion ERR");
+			} else {
+				console.log("saved data respiracion OK");
+			}
+		});
+	}
 };
--- controllers/restEventController.js
+++ controllers/restEventController.js
@@ -8,44 +8,46 @@ var sendJsonResponse = function(res, status, content){
     res.json(content);
 };
 
-module.exports.safeZoneCreate = function(req, res){
-  safezone.create({
-    petName: "temp",
-    //aproximadamente las cuatro esquinas del ML
-    //4.602732, -74.065319
-    //4.602316, -74.064677
-    //4.602832, -74.064416
-    //4.603182, -74.064966
-    boundary: [
-      [4.602732, -74.065319],
-      [4.602316, -74.064677],
-      [4.602832, -74.064416],
-      [4.603182, -74.064966]
-    ]
-  }, function(err, data){
-    if(err){
-        sendJsonResponse(res, 400, err);
-    } else {
-        console.log("saved data");
-        sendJsonResponse(res, 201, data);
-    }
-  });
+module.exports = {
+    safeZoneCreate:  function(req, res){
+        safezone.create({
+            petName: "temp",
+            //aproximadamente las cuatro esquinas del ML
+            //4.602732, -74.065319
+            //4.602316, -74.064677
+            //4.602832, -74.064416
+            //4.603182, -74.064966
+            boundary: [
+                [4.602732, -74.065319],
+                [4.602316, -74.064677],
+                [4.602832, -74.064416],
+                [4.603182, -74.064966]
+            ]
+        }, function(err, data){
+            if(err){
+                sendJsonResponse(res, 400, err);
+            } else {
+                console.log("saved data");
+                sendJsonResponse(res, 201, data);
+            }
+        });
 
-};
+    },
 
 //Servicio REST para pruebas
-module.exports.positionEventCreate = function(req, res){
+    positionEventCreate: function(req, res){
 
-    positionEvent.create({
-	        petName: "temp",
-	        coords: [parseFloat(10), parseFloat(20)],
-	    }, function(err, location){
-	        if(err){
-	            sendJsonResponse(res, 400, err);
-	        } else {
-	            console.log("saved data");
-	            sendJsonResponse(res, 201, location);
-	        }
-	    });
-    // console.log('hello');
-	};
+        positionEvent.create({
+            petName: "temp",
+            coords: [parseFloat(10), parseFloat(20)],
+        }, function(err, location){
+            if(err){
+                sendJsonResponse(res, 400, err);
+            } else {
+                console.log("saved data");
+                sendJsonResponse(res, 201, location);
+            }
+        });
+        // console.log('hello');
+    }
+}
